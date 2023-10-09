b551fcb1587f559ce967bc558b6f7bd5621c90f9
nahuepereyraa <127611645+nahuepereyraa@users.noreply.github.com>
2023-10-09 13:02:46 -0300

    cambios 2

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import {  } from "../data";
 
 const ProductList = () => {
     return (
-        <div>
-            ProductList
+        <div className='container-items'> 
+        {data.map(Product => (
+            <div className='item'>
+                <figure>
+                    <img src={Product.img} alt={Product.nameProduct} />
+                </figure>
+                <div className='info-product'>
+                    <h2>{Product.nameProduct}</h2>
+                    <p class="price">${Product.price}</p>
+                    <button>Añadir al carrito</button>
+                </div>
+            </div>
+            ))}        
         </div>
     );
 };
