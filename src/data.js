ef8a0765759665d262c3c45333b195a45a575656
nahuepereyraa <127611645+nahuepereyraa@users.noreply.github.com>
2023-10-09 12:37:36 -0300

    Header, ProductList y indexcss

---
+++ src/data.js
@@ -0,0 +1,30 @@
+export const data = [
+    {
+        id: 1, 
+        img: "https://images.unsplash.com/photo-1518002171953-a080ee817e1f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" ,
+        nameProduct: "zapatillas adidas",
+        price: 100,
+        quantity: 1,
+    },    
+    {
+        id: 2, 
+        img: "https://images.unsplash.com/photo-1521093470119-a3acdc43374a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
+        nameProduct: "zapatillas nike",
+        price: 110,
+        quantity: 1,
+    },
+    {
+        id: 3, 
+        img: "https://images.unsplash.com/photo-1619253341026-74c609e6ce50?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"  ,
+        nameProduct: "zapatillas puma",
+        price: 90,
+        quantity: 1,
+    },
+    {
+        id: 4, 
+        img: "https://images.unsplash.com/photo-1680204101574-dea570724119?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"  ,
+        nameProduct: "zapatillas converse",
+        price: 50,
+        quantity: 1,
+    },
+]
